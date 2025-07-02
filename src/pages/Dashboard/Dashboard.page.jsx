0847c7f91709eaeaeba8866de394430fcc80457d
Ruben-Garrido-Piquer <rubengarrido241@gmail.com>
2025-07-02 13:41:03 +0200

    Dashborad base

--- src/pages/Dashboard/Dashboard.page.jsx
+++ src/pages/Dashboard/Dashboard.page.jsx
@@ -1,39 +1,39 @@
 import { useEffect, useState } from "react";
 import { Flex, Typography } from "antd";
 import { Ingredient } from "../../components/Ingredient/Ingredient";
-
 const { Title } = Typography;
 
 const Dashboard = () => {
-    const [ingredients, setIngredients] = useState([]);
+  const [ingredients, setIngredients] = useState([]);
 
-    useEffect(() => {
-        const accessToken = localStorage.getItem("accessToken");
-        fetch("http://localhost:8080/ingredients", {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${accessToken}`,
-            },
-        })
-            .then((res) => {
-                if (!res.ok) throw new Error("Failed to load ingredients");
-                return res.json();
-            })
-            .then((data) => setIngredients(data))
-            .catch(console.error);
-    }, []);
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+    fetch("http://localhost:8080/ingredients", {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+        Authorization: accessToken,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setIngredients(data);
+      });
+  }, []);
 
-    return (
-        <Flex direction="column" gap="10px" style={{ padding: "1rem" }}>
-            <Title level={2}>Dashboard</Title>
-            <Flex wrap="wrap" gap="10px">
-                {ingredients.map((ing) => (
-                    <Ingredient key={ing.id} name={ing.name} />
-                ))}
-            </Flex>
-        </Flex>
-    );
+  return (
+    <Flex vertical gap={"5px"}>
+      <Title>Dashboard</Title>
+      {ingredients.map((ingredient, index) => {
+        return (
+          <Ingredient
+            name={ingredient.name}
+            key={index}
+          />
+        );
+      })}
+    </Flex>
+  );
 };
 
 export { Dashboard };
