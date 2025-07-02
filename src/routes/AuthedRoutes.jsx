7eadcfcee9c8986f7522461c90ca3f3afa1528fa
Tomas-crypto14 <tomasignaciocapalbo@gmail.com>
2025-07-02 13:26:36 +0200

    Actualizado

--- src/routes/AuthedRoutes.jsx
+++ src/routes/AuthedRoutes.jsx
@@ -1,14 +1,23 @@
 import { Route, Routes, Navigate } from "react-router";
 import { Dashboard } from "../pages/Dashboard/Dashboard.page";
-
+import { Ingredient } from "../components/Ingredient/Ingredient"
+import { useEffect } from "react";
 const AuthedRoutes = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!localStorage.getItem("access_token")) {
+      navigate("/login");
+    }
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
-      <Route path="/profile" element={<h1>Profile</h1>} />
-      <Route path="*" element={<Navigate to="/" replace />} />
+      <Route path="/ingredient" element={<Ingredient />} />
+      <Route path="*" element={<Navigate to="/ingredient" replace />} />
     </Routes>
-  );
-};
+  )
+}
 
-export { AuthedRoutes };
+export { AuthedRoutes }
