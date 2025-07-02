427f25cdcf3446c3270e810de7d5e61b20f0586e
Ruben-Garrido-Piquer <rubengarrido241@gmail.com>
2025-07-02 13:39:35 +0200

    App.jsx base

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router";
 import { UnAuthedRoutes } from "./routes/UnAuthedRoutes";
 import { AuthedRoutes } from "./routes/AuthedRoutes";
 import "./App.css";
 
 function App() {
-    const userLoggedIn = localStorage.getItem("accessToken");
+  const userLoggedIn = localStorage.getItem("accessToken")
 
-    return (
-        <BrowserRouter>
-            {userLoggedIn ? <AuthedRoutes /> : <UnAuthedRoutes />}
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      {userLoggedIn ? <AuthedRoutes /> : <UnAuthedRoutes />}
+    </BrowserRouter>
+  );
 }
 
 export default App;
