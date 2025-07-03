ac9d6bdfdb9012921c687efa5c1be67d38a26680
Joan <joan1998bocairent@gmail.com>
2025-07-03 13:24:10 +0200

    Better Styles IX to Ingredients View

--- src/routes/AuthedRoutes.jsx
+++ src/routes/AuthedRoutes.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes, Navigate } from "react-router";
 import { Dashboard } from "../pages/Dashboard/Dashboard.page";
+import { Ingredients } from "../pages/Ingredients/Ingredients.jsx";
 
 const AuthedRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
       <Route path="/profile" element={<h1>Profile</h1>} />
+      <Route path="/pantry" element={<Ingredients />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
--- src/routes/UnAuthedRoutes.jsx
+++ src/routes/UnAuthedRoutes.jsx
@@ -1,14 +1,12 @@
 import { Route, Routes, Navigate } from "react-router";
 import { Login } from "../pages/Login/Login.page";
 import { Register } from "../pages/Register/Register.page";
-import { Ingredients } from "../pages/Ingredients/Ingredients.jsx";
 
 const UnAuthedRoutes = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/pantry" element={<Ingredients />} />
       <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   );
