c9146e8a3921e435f51d105d838ec0fd49c2c5ce
= <=>
2025-07-04 11:33:46 +0200

    Ingredient component

--- src/components/molecules/Ingredient/Ingredient.jsx
+++ src/components/molecules/Ingredient/Ingredient.jsx
@@ -1,5 +1,16 @@
 import "./Ingredient.css";
+import ingredientIcon from "../../../assets/ingredienticon.svg";
 
-const Ingredient = ({ name }) => <div className="ingredient-card">{name}</div>;
+const Ingredient = ({ name, quantity, unit }) => (
+  <div className="ingredient-card">
+    <div className="ingredient-card-icon">
+      <img src={ingredientIcon} alt="Icon of ingredient" />
+    </div>
+    <div className="ingredient-card-info">
+      <h4>{name}</h4>
+      {quantity ? <h5>{quantity} left</h5> : unit ? <h5>{unit} g left</h5> : ""}
+    </div>
+  </div>
+);
 
 export { Ingredient };
